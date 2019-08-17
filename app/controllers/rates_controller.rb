51aaf3aa9a2788b8647805b11587b89ab39c81cd
Carlos <cortega@vailsys.com>
2019-08-17 17:53:27 -0500

    adds post and get endpoint for rates

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,51 +1,70 @@
-class RatesController < ApplicationController
-  before_action :set_rate, only: [:show, :update, :destroy]
+require 'structure_validator'
 
+class RatesController < ApplicationController
   # GET /rates
   def index
-    @rates = Rate.all
+    @rates = Rate.order(:price, :start, :end, :timezone).to_a
 
-    render json: @rates
-  end
+    groups = []
+    group_idx = -1
+    start_time = nil
+    end_time = nil
+    price = nil
+    timezone = nil
+    # seperate by price start and end
+    @rates.each do | rate |
+      if rate.start == start_time && rate.end == end_time && rate.price == price && rate.timezone == timezone
+        groups[group_idx] << rate
+      else
+        group_idx += 1
+        groups << [rate]
+        start_time = rate.start
+        end_time = rate.end
+        price = rate.price
+        timezone = rate.timezone
+      end
+    end
 
-  # GET /rates/1
-  def show
-    render json: @rate
+    formatted_groups = []
+    groups.each do | group |
+      formatted_groups << StructureValidator.restructure(group)
+    end
+
+    render json: {rates: formatted_groups}
   end
 
   # POST /rates
   def create
-    @rate = Rate.new(rate_params)
+    rates = JSON.parse(request.body.read).symbolize_keys[:rates]
 
-    if @rate.save
-      render json: @rate, status: :created, location: @rate
-    else
-      render json: @rate.errors, status: :unprocessable_entity
+    status = :created
+    error = nil
+    rates_to_save = []
+    rates.each do | group |
+      group.symbolize_keys!
+
+      # basic error checking
+      if !StructureValidator::all_valid?(group)
+        status = :unprocessable_entity
+        error = "The structure is incorrect"
+        break
+      end
+
+      created_results = Rate.new_from_group(group)
+      if created_results[:error]
+        status = :unprocessable_entity
+        error = "One or more fields are not valid"
+        break
+      else
+        rates_to_save = rates_to_save + created_results[:rates]
+      end
     end
+
+    if !error
+      Rate.replace_all(rates_to_save)
+    end
+
+    render json: {error: error}, status: status
   end
 
-  # PATCH/PUT /rates/1
-  def update
-    if @rate.update(rate_params)
-      render json: @rate
-    else
-      render json: @rate.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /rates/1
-  def destroy
-    @rate.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_rate
-      @rate = Rate.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def rate_params
-      params.require(:rate).permit(:day_key, :price, :start, :end)
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_024326) do
+ActiveRecord::Schema.define(version: 2019_08_15_165202) do
 
   create_table "rates", force: :cascade do |t|
     t.text "day_key"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_024326) do
     t.time "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "timezone"
   end
 
 end
